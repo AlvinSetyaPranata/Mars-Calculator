a89d02e1f2c5ac2cce46d5448ea0642f0fbb079b
AlvinSetyaPranata <command.alvin@gmail.com>
2023-03-18 21:07:25 +0700

    Created base layout for view scroll

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,64 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Mars Calculator',
-          ),
-          leading: IconButton(
-            icon: Icon(Icons.settings, color: Colors.white),
-            onPressed: () {},
-          ),
-          backgroundColor: Colors.black,
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Calculator(),
+      theme: ThemeData(scaffoldBackgroundColor: Colors.grey[900]),
+    );
+  }
+}
+
+class Calculator extends StatefulWidget {
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  Widget Button() {
+    return Container(
+        child: ElevatedButton(
+      onPressed: null,
+      child: null,
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mars Calculator'),
+        backgroundColor: Colors.black,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: EdgeInsetsDirectional.all(10.0),
+                  child: Text(
+                    '0',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(color: Colors.white, fontSize: 100),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [],
+            )
+          ],
         ),
       ),
-    ));
+    );
+  }
+}
